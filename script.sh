62c006958e54d0a3dc7642154ea469882d81c44a
Igor A. Vilhalba <igor.vilhalba@g.globo>
2024-11-13 00:25:33 -0300

    refactor script

--- script.sh
+++ script.sh
@@ -1,10 +1,16 @@
-magick peppers.tiff -resize 256x256! -colorspace Gray -depth 8 BMP3:peppers.bmp
-convert peppers.bmp \
-	-define convolve:scale='!' \
+function dst_name() {
+	echo "${1%.*}-sobel-thin.bmp"
+}
+
+magick peppers.tiff \
+	-resize 256x256! \
+	-colorspace Gray \
+	-depth 8 BMP3:- \
+	| magick - -define convolve:scale='!' \
 	-define morphology:compose=Lighten \
 	-morphology Convolve  'Sobel:>' \
 	-threshold 30% \
-	-type Bilevel \
-	BMP3:peppers-sobel.bmp
-
-convert peppers-sobel.bmp -morphology Thinning:-1 Skeleton:1 -negate BMP3:peppers-sobel-thin.bmp
+	-type Bilevel BMP3:- \
+	| magick - -morphology Thinning:-1 Skeleton:1 \
+	-negate \
+	BMP3:$(dst_name $1)
